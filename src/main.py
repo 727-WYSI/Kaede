e1d7716aa82814c54acb5b2a7b7d2d6e22262b5b
VietLizi <86664792+vietlizi@users.noreply.github.com>
2024-03-27 01:09:34 +0700

    update: he can now tell the time

--- src/main.py
+++ src/main.py
@@ -1,64 +1,72 @@
 import discord
 from discord.ext import commands
 import random
+import os
+from dotenv import load_dotenv
+from time.time_functions import get_time  # Import the get_time function
 
-TOKEN = 'INSERT_YOUR_BOT_TOKEN'
-PREFIX = '!' #replaced if needed
-CHANNEL_ID = INSERT_YOUR_CHANNEL_ID
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+PREFIX = '?'  # Prefix for your bot commands, change as needed
+CHANNEL_ID = YOUR_CHANNEL_ID
+WELCOME_FOLDER = 'welcome_memes'
+GOODBYE_FOLDER = 'goodbye_memes'
+TIME_FOLDER = 'time'
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-#Memes list
-def get_memes():
-    meme_files = os.listdir(MEMES_FOLDER)
-    return [os.path.join(MEMES_FOLDER, file) for file in meme_files]
+# Function to get list of filenames from the folder
+def get_files(folder):
+    files = os.listdir(folder)
+    return [os.path.join(folder, file) for file in files]
 
 @bot.event
 async def on_ready():
-    print(f'Login successfully as {bot.user}')
+    print(f'We have logged in as {bot.user}')
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(CHANNEL_ID)
 
     if channel is not None:
-        welcome_message = (
-            f'Hello {member.mention}! '
-            f'Welcome to {member.guild.name}! '
-            f'We hope you have an adventurous time here.\n'
-            f'Here\'s a random meme for you:\n{random.choice(welcome_memes)}'
-        )
-#Funny music link section
-        funny_music_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Rick Astley - Never Gonna Give You Up
-        funny_music_message = (
-            f'But first, let\'s get into the mood with a funny music: {funny_music_link}'
-        )
+        welcome_memes = get_files(WELCOME_FOLDER)
+        if welcome_memes:
+            meme_path = random.choice(welcome_memes)
+            welcome_message = (
+                f'Hello {member.mention}! '
+                f'Welcome to {member.guild.name}! '
+                f'We hope you have an adventurous time here.\n'
+            )
+            # Send the welcome message with a random welcome meme
+            await channel.send(welcome_message, file=discord.File(meme_path))
 
-        # Send the messages to the channel
-        await channel.send(welcome_message)
-        await channel.send(funny_music_message)
+            # Get and send the current time for a city
+            time_city = 'America/New_York'  # Change to the desired city or timezone
+            current_time = get_time(time_city)
+            time_message = f'Current Time in {time_city}: {current_time}'
+            await channel.send(time_message)
 
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(CHANNEL_ID)
 
     if channel is not None:
-        goodbye_message = (
-            f'Goodbye, {member.display_name}! We will miss you. '
-            f'Here\'s a sad meme to remember our time together:\n{random.choice(goodbye_memes)}'
-        )
+        goodbye_memes = get_files(GOODBYE_FOLDER)
+        if goodbye_memes:
+            meme_path = random.choice(goodbye_memes)
+            goodbye_message = (
+                f'Goodbye, {member.display_name}! We will miss you. '
+                f'Here\'s a meme to remember our time together:\n'
+            )
+            await channel.send(goodbye_message, file=discord.File(meme_path))
 
-        sad_music_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Rick Astley - Never Gonna Give You Up
-        sad_music_message = (
-            f'And to make it even more sentimental, here\'s a sad music: {sad_music_link}'
-        )
-
-        # Send the messages to the channel
-        await channel.send(goodbye_message)
-        await channel.send(sad_music_message)
+            # Get and send the current time for a city
+            time_city = 'America/New_York'  # Change to the desired city or timezone
+            current_time = get_time(time_city)
+            time_message = f'Current Time in {time_city}: {current_time}'
+            await channel.send(time_message)
 
 @bot.command(name='owo', help='Responds with owo')
 async def owo(ctx):
